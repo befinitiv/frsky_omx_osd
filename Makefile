6a1537b6f2f332b2d6419df3025faa8093c7b0a5
befi <befi@localhost>
2015-06-26 22:31:16 +0000

    added graphics stuff to Makefile and drawed an initial line

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
+
+VGFONTPATH=/opt/vc/src/hello_pi/libs/vgfont
+HELLOFONTPATH=/opt/vc/src/hello_pi/hello_font/
+
+CPPFLAGS+= -I/opt/vc/include/ -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -I$(VGFONTPATH)
+LDFLAGS+= -L$(VGFONTPATH) -lvgfont -lfreetype -lz
+LDFLAGS+=-L/opt/vc/lib/ -lGLESv2 -lEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm -lpthread -lrt -lm
+
 all: frsky_omx_osd
 
+vgfont:
+	cp $(HELLOFONTPATH)/Vera.ttf .
+	make -C $(VGFONTPATH)
 
-
-frsky_omx_osd: main.c
-	gcc -Wall main.c -o frsky_omx_osd
+frsky_omx_osd: vgfont main.c
+	gcc -Wall $(CPPFLAGS) main.c -o frsky_omx_osd $(LDFLAGS)
 
 
 clean:
--- main.c
+++ main.c
@@ -1,7 +1,41 @@
-#include <stdio.h>
+/*
+Copyright (c) 2015, befinitiv
+Copyright (c) 2012, Broadcom Europe Ltd
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of the copyright holder nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CA
+
+#include <stdio.h>ND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
 #include <stdint.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+
+#include "bcm_host.h"
+#include "vgfont.h"
+
 
 #include "frsky.h"
 
@@ -126,15 +160,31 @@ void parse_frsky_buffer(uint8_t ch) {
 
 
 int main(void) {
+	GRAPHICS_RESOURCE_HANDLE img;
+	uint32_t width, height;
+	int LAYER=1;
+	bcm_host_init();
+	int s;
+
 	uint8_t buf[256];
 	size_t n;
+
+	gx_graphics_init(".");
+	graphics_get_display_size(0, &width, &height);
+	gx_create_window(0, width, height, GRAPHICS_RESOURCE_RGBA32, &img);
+	// transparent before display to avoid screen flash
+	graphics_resource_fill(img, 0, 0, width, height, GRAPHICS_RGBA32(0,0,0,0x00));
+	graphics_display_resource(img, 0, LAYER, 0, 0, GRAPHICS_RESOURCE_WIDTH, GRAPHICS_RESOURCE_HEIGHT, VC_DISPMAN_ROT0, 1);
+
+graphics_resource_fill(img, 0, height-40, width, 1, GRAPHICS_RGBA32(0,0xff,0,0xff));
+graphics_update_displayed_resource(img, 0, 0, 0, 0);
 
 
 	for(;;) {
 		n = read(STDIN_FILENO, buf, sizeof(buf));
 
 		if(n == 0) {
-			break; //EOF
+		//	break; //EOF
 		}
 
 		if(n<0) {
